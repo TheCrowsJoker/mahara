e953685499933f4dd28245029304a25d49c498ad
Naomi Guyer <naomi@catalyst.net.nz>
2015-01-08 16:52:03 +1300

    Navigation Applied bootstrap tabs and tidy up form styling Mobile menu Add bootstrap accessibility, tab styling to bootstrap
    Change-Id: Ifa42d482f5fd7e1cdfb52bae1ebb5ff3bf5085a9

--- htdocs/lang/en.utf8/mahara.php
+++ htdocs/lang/en.utf8/mahara.php
@@ -638,7 +638,7 @@ $string['email']                    = 'Email';
 $string['emails']                    = 'Emails';
 $string['subject']                  = 'Subject';
 $string['message']                  = 'Message';
-$string['messageoptional']          = 'Message <span class="accessible-hidden">(optional)</span>';
+$string['messageoptional']          = 'Message <span class="accessible-hidden sr-only">(optional)</span>';
 $string['messagesent']              = 'Your message has been sent';
 $string['nosendernamefound']        = 'No sender name was submitted';
 $string['emailnotsent']             = 'Failed to send contact email. Error message: "%s"';
@@ -1183,4 +1183,3 @@ $string['vspace'] = 'Vertical space';
 $string['hspace'] = 'Horizontal space';
 $string['border'] = 'Border width';
 $string['alignment'] = 'Alignment';
-
