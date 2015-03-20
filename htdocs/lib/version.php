c3a5b968e4670b1e428627b7d455347447b3561f
Robert Lyon <robertl@catalyst.net.nz>
2015-03-20 16:18:02 +1300

    Version bump for 15.04rc2dev
    Signed-off-by: Robert Lyon <robertl@catalyst.net.nz>

--- htdocs/lib/version.php
+++ htdocs/lib/version.php
@@ -17,10 +17,10 @@ $config = new stdClass();
 // For upgrades on stable branches, increment the version by one.  On master, use the date.
 
 
+
 $config->version = 2015041400;
 $config->series = '15.10';
 $config->release = '15.10dev';
-
 
 $config->minupgradefrom = 2009022600;
 $config->minupgraderelease = '1.1.0 (release tag 1.1.0_RELEASE)';
