42861f4867046354876efa9349e0e1e1fa5c75b9
Naomi Guyer <naomi@catalyst.net.nz>
2015-05-26 12:00:22 +1200

    Page Builder and BUG fixes (bootstrap)
    Change-Id: I332f8f2ea0c317b5171fe9163912c760f6bc2c77

--- htdocs/import/index.php
+++ htdocs/import/index.php
@@ -86,6 +86,7 @@ switch ($action) {
 function print_upload_form() {
 
     $form = pieform(array(
+        'class' => 'mtxl ptxl',
         'name'        => 'import',
         'method'      => 'post',
         'plugintype ' => 'core',
@@ -99,6 +100,7 @@ function print_upload_form() {
                 )
             ),
             'submit' => array(
+                'class' => 'btn btn-success',
                 'type'  => 'submit',
                 'value' => get_string('Import', 'import'),
             ),
